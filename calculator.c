ae80ab0dc1bbf8d02272af1d1aedd9f239a3aca9
Dominik Ukalski <just.a.frogrammer@gmail.com>
2024-11-06 01:06:22 +0100

    Beninging

---
+++ calculator.c
@@ -0,0 +1,17 @@
+/*
+
+git add calculator.c
+git commit -m "Description of changes" - to update git project
+
+gcc calculator.c -o calculator
+./calculator                           - to compile
+
+
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    printf("This will be a simple calculator in the future");
+}
